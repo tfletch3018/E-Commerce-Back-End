5184c6e3e6b4a67c5ec91d92282b22c0c2f58130
Tonya Fletcher <tfletch4504@gmail.com>
2021-04-11 10:30:45 -0400

    all routes connected

--- models/index.js
+++ models/index.js
@@ -21,7 +21,7 @@ Product.belongsToMany(Tag, {
 });
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(ProductTag, {
+Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: 'tag_id'
 });
